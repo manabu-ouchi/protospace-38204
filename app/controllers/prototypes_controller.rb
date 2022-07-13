879536ed555a551546d95d7322533b73345e6d53
manabu-ouchi <mochi.mochi.pillow@gmail.com>
2022-07-13 14:07:42 +0900

    投稿者以外のユーザーが、投稿者専用のページに遷移できない

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -76,7 +76,8 @@ class PrototypesController < ApplicationController
 
 
   def move_to_index
-    unless user_signed_in?
+    prototype = Prototype.find(params[:id])
+    unless user_signed_in? && current_user.id == prototype.user_id 
       redirect_to root_path
     end
   end
